90c61cfbbcd77b9e6645fdae35f3fc64bd518cf9
Griffo525 <sam.griffiths94@gmail.com>
2018-06-15 10:32:16 +0100

    Added background animation.

--- app/src/main/java/com/chaty/samg/chaty/WelcomeActivity.java
+++ app/src/main/java/com/chaty/samg/chaty/WelcomeActivity.java
@@ -1,13 +1,53 @@
 package com.chaty.samg.chaty;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    AnimationDrawable animationDrawable;
+
+    RelativeLayout relativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        relativeLayout = (RelativeLayout)findViewById(R.id.relativeLayout);
+
+        animationDrawable = (AnimationDrawable) relativeLayout.getBackground();
+
+        animationDrawable.setEnterFadeDuration(5000);
+
+        animationDrawable.setExitFadeDuration(2000);
+    }
+
+    @Override
+
+    protected void onResume() {
+
+        super.onResume();
+
+        if (animationDrawable != null && !animationDrawable.isRunning())
+
+            animationDrawable.start();
+
+    }
+
+
+
+    @Override
+
+    protected void onPause() {
+
+        super.onPause();
+
+        if (animationDrawable != null && animationDrawable.isRunning())
+
+            animationDrawable.stop();
+
     }
 }
