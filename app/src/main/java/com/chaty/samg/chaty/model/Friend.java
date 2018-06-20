415eeb08987a52b358c7dc7f7560c52e21b85b9b
Griffo525 <sam.griffiths94@gmail.com>
2018-06-20 14:29:18 +0100

    Full commit.

---
+++ app/src/main/java/com/chaty/samg/chaty/model/Friend.java
@@ -0,0 +1,8 @@
+package com.chaty.samg.chaty.model;
+
+
+
+public class Friend extends User{
+    public String id;
+    public String idRoom;
+}
---
+++ app/src/main/java/com/chaty/samg/chaty/model/Group.java
@@ -0,0 +1,12 @@
+package com.chaty.samg.chaty.model;
+
+
+
+public class Group extends Room{
+    public String id;
+    public ListFriend listFriend;
+
+    public Group(){
+        listFriend = new ListFriend();
+    }
+}
